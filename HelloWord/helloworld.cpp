6c27f0a0a90514560248882eb8ec6e55f9c5268f
BuildTools <unconfigured@null.spigotmc.org>
2024-11-19 21:45:12 -0500

    First commit

---
+++ HelloWord/helloworld.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int age = 25;
+    float height = 5.9;
+    char grade = 'A';
+    string name = "John";
+
+    cout << "Age:" << age << endl;
+    cout << "Height:" << height << endl;
+    cout << "Grade:" << grade << endl;
+    cout << "Name:" << name << endl;
+
+    std::cout << "Hello world!" << std::endl;
+    return 0;
+}
+
+// test
+
+
